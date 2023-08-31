bcd59a2538b5feef894751cb7036dda33023a77d
atharvabe19e <atharvabelge@gmail.com>
2023-08-31 11:40:35 +0530

    Normal XO for now

---
+++ App.js
@@ -0,0 +1,28 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import {NavigationContainer} from '@react-navigation/native'
+import GameScreen from './screens/GameScreen'
+
+const Stack=createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <>
+    <SafeAreaView style={styles.all}>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}} >
+        <Stack.Screen name='Game' component={GameScreen}  />
+      </Stack.Navigator>
+    </NavigationContainer>
+    </SafeAreaView>
+    </>
+  )
+
+}
+
+export default App
+
+const styles = StyleSheet.create({all:{
+  flex:1
+}})
---
+++ screens/GameScreen.js
@@ -0,0 +1,272 @@
+import { Image, TouchableOpacity, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import {Dimensions} from 'react-native';
+const windowWidth = Dimensions.get('window').width;
+
+const GameScreen = () => {
+    const [active_player, setActive_player] = useState('X')
+    const [markers, setMarkers] = useState([
+      null, null, null,
+      null, null, null,
+      null, null, null
+    ])
+
+    // Add marker on clicked position
+    const markPosition = (position) => {
+      if(!markers[position]){
+        let temp = [...markers]
+        temp[position] = active_player
+        setMarkers(temp)
+        if(active_player === 'X'){  //transfer chances to next player
+          setActive_player('O')
+        }else{
+          setActive_player('X')
+        }
+      }
+    }
+
+    // Clear entire board
+    const resetMarkers = () => {
+      setMarkers([
+        null, null, null,
+        null, null, null,
+        null, null, null
+      ])
+      setActive_player('X')
+    }
+
+    //Function to calculate winner
+    const calculateWinner = (squares) => {
+      const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+      ];
+      for(let i = 0; i < lines.length; i++){
+        const [a,b,c] = lines[i];
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+          return squares[a];
+        }
+      }
+      return null;
+    }
+    
+
+    //UseEffect to call each time any player plays 
+    useEffect(() => {
+      const winner = calculateWinner(markers);
+      if(winner === 'X'){
+        alert("Player X Won!")
+        resetMarkers()
+      }else if(winner === 'O'){
+        alert("Player O Won!")
+        resetMarkers()
+      }
+    }, [markers])
+
+
+  return (
+    <SafeAreaView style={styles.all}>
+        <View style={[styles.playerInfo,{backgroundColor:active_player==='X'?'#007FF4':'#F40075'}]}>
+            <Text style={styles.playerTxt}>
+                Player X's turn            
+            </Text>
+        </View>    
+        <View style={styles.mainContainer}> 
+        
+        {/* Top Left Cell */}
+        <TouchableOpacity style={[styles.cell,styles.cellR,styles.cellD]} onPress={()=>markPosition(0)}>
+          {markers[0] === 'X' && <Image source={require('../assets/img/cross.png')} style={styles.icon} />}
+          {markers[0] === 'O' && <Image source={require('../assets/img/zero.png')} style={styles.icon} />}
+        </TouchableOpacity>
+
+        {/* Top Mid Cell */}
+        <TouchableOpacity style={[styles.cell,styles.cellD]} onPress={()=>markPosition(1)}>
+          {markers[1] === 'X' && <Image source={require('../assets/img/cross.png')} style={styles.icon} />}
+          {markers[1] === 'O' && <Image source={require('../assets/img/zero.png')} style={styles.icon} />}
+        </TouchableOpacity>
+
+        {/* Top Right Cell */}
+        <TouchableOpacity style={[styles.cell,styles.cellD,styles.cellL]} onPress={()=>markPosition(2)}>
+          {markers[2] === 'X' && <Image source={require('../assets/img/cross.png')} style={styles.icon} />}
+          {markers[2] === 'O' && <Image source={require('../assets/img/zero.png')} style={styles.icon} />}
+        </TouchableOpacity>
+
+        {/* Mid Left Cell */}
+        <TouchableOpacity style={[styles.cell,styles.cellR]} onPress={()=>markPosition(3)}>
+          {markers[3] === 'X' && <Image source={require('../assets/img/cross.png')} style={styles.icon} />}
+          {markers[3] === 'O' && <Image source={require('../assets/img/zero.png')} style={styles.icon} />}
+        </TouchableOpacity>
+
+        {/* Mid Mid Cell */}
+        <TouchableOpacity style={styles.cell} onPress={()=>markPosition(4)}>
+          {markers[4] === 'X' && <Image source={require('../assets/img/cross.png')} style={styles.icon} />}
+          {markers[4] === 'O' && <Image source={require('../assets/img/zero.png')} style={styles.icon} />}
+        </TouchableOpacity>
+
+        {/* Mid Right Cell */}
+        <TouchableOpacity style={[styles.cell,styles.cellL]} onPress={()=>markPosition(5)}>
+          {markers[5] === 'X' && <Image source={require('../assets/img/cross.png')} style={styles.icon} />}
+          {markers[5] === 'O' && <Image source={require('../assets/img/zero.png')} style={styles.icon} />}
+        </TouchableOpacity>
+
+        {/* Bottom Left Cell */}
+        <TouchableOpacity style={[styles.cell,styles.cellR,styles.cellU]} onPress={()=>markPosition(6)}>
+          {markers[6] === 'X' && <Image source={require('../assets/img/cross.png')} style={styles.icon} />}
+          {markers[6] === 'O' && <Image source={require('../assets/img/zero.png')} style={styles.icon} />}
+        </TouchableOpacity>
+
+        {/* Bottom Mid Cell */}
+        <TouchableOpacity style={[styles.cell,styles.cellU]} onPress={()=>markPosition(7)}>
+          {markers[7] === 'X' && <Image source={require('../assets/img/cross.png')} style={styles.icon} />}
+          {markers[7] === 'O' && <Image source={require('../assets/img/zero.png')} style={styles.icon} />}
+        </TouchableOpacity>
+
+        {/* Bottom Right Cell */}
+        <TouchableOpacity style={[styles.cell,styles.cellL,styles.cellU]} onPress={()=>markPosition(8)}>
+          {markers[8] === 'X' && <Image source={require('../assets/img/cross.png')} style={styles.icon} />}
+          {markers[8] === 'O' && <Image source={require('../assets/img/zero.png')} style={styles.icon} />}
+        </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity style={styles.cancleBTN} onPress={resetMarkers}>
+          <Image source={require('../assets/img/replay.png')} style={styles.cancelIcon}/>
+        </TouchableOpacity>
+
+    </SafeAreaView>
+  )
+}
+
+export default GameScreen
+
+const styles = StyleSheet.create({all:{
+    flex:1,
+    backgroundColor:'#fff'
+},
+playerInfo: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    paddingVertical: 20,
+    marginTop: 30,
+    borderRadius:40
+  },
+  playerTxt: {
+    fontSize: 20,
+    fontWeight: '700',
+    letterSpacing: 1.2,
+    color: '#fff'
+  },
+  mainContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    marginTop: 60
+  },
+  cell:{  width: windowWidth / 3.2,
+  height: windowWidth / 3.2,
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',}
+  ,
+  cellR:{borderRightWidth:6},
+  cellL:{borderLeftWidth:6},
+  cellD:{borderBottomWidth:6},
+  cellU:{borderTopWidth:6},
+
+  /* cell_top_left: {
+    width: windowWidth / 3.2,
+    height: windowWidth / 3.2,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRightWidth: 6,
+    borderBottomWidth: 6,
+  },
+  cell_top_mid: {
+    width: windowWidth / 3.2,
+    height: windowWidth / 3.2,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 6
+  },
+  cell_top_right: {
+    width: windowWidth / 3.2,
+    height: windowWidth / 3.2,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 6,
+    borderLeftWidth: 6,
+  },
+  cell_mid_left: {
+    width: windowWidth / 3.2,
+    height: windowWidth / 3.2,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRightWidth: 6,
+  },
+  cell_mid_mid: {
+    width: windowWidth / 3.2,
+    height: windowWidth / 3.2,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cell_mid_right: {
+    width: windowWidth / 3.2,
+    height: windowWidth / 3.2,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderLeftWidth: 6,
+  },
+  cell_bottom_left: {
+    width: windowWidth / 3.2,
+    height: windowWidth / 3.2,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRightWidth: 6,
+    borderTopWidth: 6,
+  },
+  cell_bottom_mid: {
+    width: windowWidth / 3.2,
+    height: windowWidth / 3.2,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderTopWidth: 6,
+  },
+  cell_bottom_right: {
+    width: windowWidth / 3.2,
+    height: windowWidth / 3.2,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderLeftWidth: 6,
+    borderTopWidth: 6,
+  } */
+  
+  icon: {
+    height: 62,
+    width: 62
+  },
+  cancleBTN: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20
+  },
+  cancelIcon: {
+    height: 80,
+    width: 80
+  }
+})
